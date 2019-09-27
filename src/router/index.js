45547acc0dd322244378ff85d669d1035f105b78
Oleeeee <Oleeeee@trabson.ball.com>
2019-09-27 14:36:20 -0300

    Commit inicial

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+﻿import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Blank from "@/views/Blank.vue";
+import Blank2 from "@/views/Blank2.vue";
+Vue.use(VueRouter);
+
+// TODO Web Template Studio: Add routes for your new pages here.
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/Blank2", component: Blank2 },
+    { path: "/Blank", component: Blank },
+
+    { path:"/", redirect: "/Blank" }
+  ]
+});
